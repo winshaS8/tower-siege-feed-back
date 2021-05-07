2a271fa08f2d2fc7a4980cfba1dc90d9616c4657
winshaS8 <72268258+winshaS8@users.noreply.github.com>
2021-05-07 21:00:52 +0530

    Add files via upload

---
+++ slingShot.js
@@ -0,0 +1,29 @@
+class slingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.01,
+            length: 0.5
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);       
+    }
+
+    fly()
+    {
+        this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA)
+        {
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }      
+    }
+    
+}
